b6f1efe2245a23058733ac228dcc04813fa0325f
Mohammad <pspipm27@gmail.com>
2025-01-02 23:08:38 +0330

    edit data file addresses

--- src/ProvincesAndCities/Countries.php
+++ src/ProvincesAndCities/Countries.php
@@ -7,14 +7,14 @@ class Countries {
     static public $germany = array();
 }
 
-$file_name = "countries/iran/provinces_and_main_cities.json";
+$file_name = __DIR__ . "../countries/iran/provinces_and_main_cities.json";
 $file = fopen($file_name, "r");
 Countries::$iran["main"] = json_decode(fread($file, filesize($file_name)));
 
-$file_name = "countries/iran/provinces_and_all_cities.json";
+$file_name = __DIR__ . "../countries/iran/provinces_and_all_cities.json";
 $file = fopen($file_name, "r");
 Countries::$iran["all"] = json_decode(fread($file, filesize($file_name)));
 
-$file_name = "countries/germany/provinces_and_all_cities.json";
+$file_name = __DIR__ . "../countries/germany/provinces_and_all_cities.json";
 $file = fopen($file_name, "r");
 Countries::$germany["all"] = json_decode(fread($file, filesize($file_name)));
