6531a58013c6a9dafaaf2a4b9bd786b68f04652d
Mohammad <pspipm27@gmail.com>
2025-01-02 23:02:37 +0330

    debug data files and add license

--- src/ProvincesAndCities/Countries.php
+++ src/ProvincesAndCities/Countries.php
@@ -3,18 +3,18 @@
 namespace ProvincesAndCities;
 
 class Countries {
-    static public $Iran = array();
-    static public $Germany = array();
+    static public $iran = array();
+    static public $germany = array();
 }
 
-$file_name = "../countries/iran/provinces_and_main_cities.json";
+$file_name = "countries/iran/provinces_and_main_cities.json";
 $file = fopen($file_name, "r");
-Countries::$Iran["main"] = json_decode(fread($file, filesize($file_name)));
+Countries::$iran["main"] = json_decode(fread($file, filesize($file_name)));
 
-$file_name = "../countries/iran/provinces_and_all_cities.json";
+$file_name = "countries/iran/provinces_and_all_cities.json";
 $file = fopen($file_name, "r");
-Countries::$Iran["all"] = json_decode(fread($file, filesize($file_name)));
+Countries::$iran["all"] = json_decode(fread($file, filesize($file_name)));
 
-$file_name = "../countries/germany/provinces_and_all_cities.json";
+$file_name = "countries/germany/provinces_and_all_cities.json";
 $file = fopen($file_name, "r");
-Countries::$Germany["all"] = json_decode(fread($file, filesize($file_name)));
+Countries::$germany["all"] = json_decode(fread($file, filesize($file_name)));
